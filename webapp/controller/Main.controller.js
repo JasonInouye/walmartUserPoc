b8f04d5873bf0330a466a6b7c78965147255f250
Jason Inouye <jason.inouye@implico.com>
2025-05-08 14:52:19 -0500

    Working user page

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
-    "use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
 
     return Controller.extend("com.walmart.walmart.user.poc.controller.Main", {
-        onInit() {
-        }
+    onInit: function() {
+      const oModel = new JSONModel();
+      oModel.loadData("model/users.json");
+      this.getView().setModel(oModel);
+    }
     });
 });
